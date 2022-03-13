8f3bce7b57bc822f0e23ff18bd445fa36c41ce38
DeVonte Ewell <devonteewell@DeVontes-MacBook-Pro.local>
2022-03-13 13:37:51 -0700

    show route is connected

--- controllers/places.js
+++ controllers/places.js
@@ -30,7 +30,18 @@ router.post('/', (req, res) => {
   res.redirect('/places')
 })
 
-
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id] })
+  }
+})
 
 router.get('/new', (req, res) => {
   res.render('places/new')
