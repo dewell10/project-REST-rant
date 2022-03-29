72aafd83a3e2acd80a2f518dda83e2d84e69f583
DeVonte Ewell <devonteewell@DeVontes-MacBook-Pro.local>
2022-03-29 12:50:53 -0700

    validation complete

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,27 +2,33 @@ const React = require('react')
 const Def = require('../default')
 
 function show(data) {
-    return (
-      <Def>
-        <main>
-          <h1>{data.place.name}</h1>
-          <img src={data.place.pic}/>
-          <h2>{data.place.city}</h2>
-          <h2>{data.place.state}</h2>
-          <h2>{data.place.cuisines}</h2>
-          <a href={`/places/${data.id}/edit`} className='btn btn-warning'>
-            Edit
-          </a>
-         
-          <form method='POST' action={`/places/${data.id}?_method=DELETE`}>
-            <button type='submit' className='btn btn-danger'>
-              Delete
-            </button>
-          </form>
-  
-        </main>
-      </Def>
-    );
-  }
+  return (<Def>
+    <main>
+      <div className="row">
+        <div className="col-sm-6">
+          <img src={data.place.pic} alt={data.place.name} />
+          <h3>
+            Located in {data.place.city}, {data.place.state}
+          </h3>
+        </div>
+        <div className="col-sm-6">
+          ...
+          <h2>
+            Description
+          </h2>
+          <h3>
+            {data.place.showEstablished()}
+          </h3>
+          <h4>
+            Serving {data.place.cuisines}
+          </h4>
+          ...
+      </div>
+      </div>
+    </main>
+  </Def>
 
-module.exports = show
+  );
+}
+
+module.exports = show;
